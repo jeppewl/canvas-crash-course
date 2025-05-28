dc1740bb2bb5ad49c0cc2c2735f707d9edd73e01
jeppewl <46894168+jeppewl@users.noreply.github.com>
2025-05-28 15:47:43 +0200

    At circa 35, basic dispersing, diminishing particles

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ function resizeAndDraw() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
+const particlesArray = [];
 
-// Call on initial load
-resizeAndDraw();
+resizeAndDraw(); // Call on initial load
 
-// Also call when window resizes
-window.addEventListener("resize", resizeAndDraw);
+window.addEventListener("resize", resizeAndDraw); // Also call when window resizes
 
 const mouse = {
   x: null,
@@ -20,30 +19,59 @@ const mouse = {
 canvas.addEventListener("click", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-  //   drawCircle();
 });
 
 canvas.addEventListener("mousemove", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-  //   drawCircle();
 });
 
-function drawCircle() {
-  ctx.fillStyle = "white";
-  // ctx.fillRect(10, 10, 150, 50);
+class Particle {
+  constructor() {
+    // this.x = mouse.x;
+    // this.y = mouse.y;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 15 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1;
+  }
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
 
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 5;
-  ctx.beginPath();
-  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-  ctx.fill();
-  ctx.stroke();
+function init() {
+  for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+init();
+console.log(particlesArray);
+
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+    if (particlesArray[i].size <= 0.3) {
+      particlesArray.splice(i, 1);
+      i--;
+    }
+  }
 }
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawCircle();
+  handleParticles();
   requestAnimationFrame(animate);
 }
 animate();
