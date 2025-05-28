25917555449fcf793de4b38fecd41a4e0ca42f58
jeppewl <46894168+jeppewl@users.noreply.github.com>
2025-05-28 18:08:04 +0200

    Before 42, constellations

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById("canvas1");
+/** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
 function resizeAndDraw() {
@@ -6,6 +7,7 @@ function resizeAndDraw() {
   canvas.height = window.innerHeight;
 }
 const particlesArray = [];
+let hue = 0;
 
 resizeAndDraw(); // Call on initial load
 
@@ -19,22 +21,29 @@ const mouse = {
 canvas.addEventListener("click", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 10; i++) {
+    particlesArray.push(new Particle());
+  }
 });
 
 canvas.addEventListener("mousemove", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 2; i++) {
+    particlesArray.push(new Particle());
+  }
 });
 
 class Particle {
   constructor() {
-    // this.x = mouse.x;
-    // this.y = mouse.y;
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
+    this.x = mouse.x;
+    this.y = mouse.y;
+    // this.x = Math.random() * canvas.width;
+    // this.y = Math.random() * canvas.height;
     this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
+    this.color = "hsl(" + hue + ",100%, 50%)";
   }
   update() {
     this.x += this.speedX;
@@ -42,21 +51,21 @@ class Particle {
     if (this.size > 0.2) this.size -= 0.1;
   }
   draw() {
-    ctx.fillStyle = "red";
+    // ctx.fillStyle = "red";
+    ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
   }
 }
 
-function init() {
-  for (let i = 0; i < 100; i++) {
-    particlesArray.push(new Particle());
-  }
-}
-
-init();
-console.log(particlesArray);
+// function init() {
+//   for (let i = 0; i < 100; i++) {
+//     particlesArray.push(new Particle());
+//   }
+// }
+// init();
+// console.log(particlesArray);
 
 function handleParticles() {
   for (let i = 0; i < particlesArray.length; i++) {
@@ -71,7 +80,12 @@ function handleParticles() {
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //   ctx.fillStyle = "rgba(0,0,0,0.05)";
+  //   ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
+  hue += 1.35;
+  hue = hue > 359 ? 0 : hue;
+  //   console.log(hue);
   requestAnimationFrame(animate);
 }
 animate();
